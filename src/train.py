8b4a96ec5cd2b44d287da7a9959967bfd255a286
Sacha-Elkoubi <sacha.elkoubi1@gmail.com>
2024-02-20 12:53:34 +0100

    path issue

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import random
+import os
 import gymnasium as gym
 from env_hiv import HIVPatient
 
@@ -88,7 +89,9 @@ class ProjectAgent:
     def save(self, path: str) -> None:
         torch.save(self.q_network.state_dict(), path)
 
-    def load(self, path: str="./model_max.pth") -> None:
+    def load(self) -> None:
+        script_path = os.path.abspath(__file__)
+        path = os.path.join(script_path, 'model_max.pth')
         self.q_network.load_state_dict(torch.load(path))
 
 def train(agent: ProjectAgent, env, episodes: int):
